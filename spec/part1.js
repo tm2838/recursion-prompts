12a214cc055ebdc411dc3e20e6635e396161f158
tingjunman <tm2838@nyu.edu>
2022-02-01 15:10:23 -0600

    11-15

--- spec/part1.js
+++ spec/part1.js
@@ -712,17 +712,17 @@
         expect(multiply(17, 5)).to.equal(17 * 5);
         expect(multiply(0, 32)).to.equal(0 * 32);
         expect(multiply(0, 0)).to.equal(0 * 0);
-        // expect(multiply(78, 453)).to.equal(78 * 453);
+        expect(multiply(78, 453)).to.equal(78 * 453);
       });
 
       it('should return the product of two negative integers', function() {
         expect(multiply(-2, -2)).to.equal(-2 * -2);
         expect(multiply(-8, -3)).to.equal(-8 * -3);
         expect(multiply(-5, -27)).to.equal(-5 * -27);
-        // expect(multiply(-79, -82)).to.equal(-79 * -82);
-        // expect(multiply(-275, -502)).to.equal(-275 * -502);
-        // expect(multiply(-12, -10)).to.equal(-12 * -10);
-        // expect(multiply(-22, -3)).to.equal(-22 * -3);
+        expect(multiply(-79, -82)).to.equal(-79 * -82);
+        expect(multiply(-275, -502)).to.equal(-275 * -502);
+        expect(multiply(-12, -10)).to.equal(-12 * -10);
+        expect(multiply(-22, -3)).to.equal(-22 * -3);
       });
 
       it('should return the product of mixed positive and negative integers', function() {
@@ -730,18 +730,18 @@
         expect(multiply(79, -82)).to.equal(79 * -82);
         expect(multiply(2, -2)).to.equal(2 * -2);
         expect(multiply(5, -27)).to.equal(5 * -27);
-        // expect(multiply(-275, 502)).to.equal(-275 * 502);
-        // expect(multiply(275, -502)).to.equal(275 * -502);
-        // expect(multiply(-8, 3)).to.equal(-8 * 3);
-        // expect(multiply(12, -10)).to.equal(12 * -10);
-        // expect(multiply(-22, 3)).to.equal(-22 * 3);
+        expect(multiply(-275, 502)).to.equal(-275 * 502);
+        expect(multiply(275, -502)).to.equal(275 * -502);
+        expect(multiply(-8, 3)).to.equal(-8 * 3);
+        expect(multiply(12, -10)).to.equal(12 * -10);
+        expect(multiply(-22, 3)).to.equal(-22 * 3);
       });
 
       it('should accept parameters in any order', function() {
         expect(multiply(2, 1)).to.equal(1 * 2);
         expect(multiply(5, 17)).to.equal(5 * 17);
         expect(multiply(32, 0)).to.equal(0 * 32);
-        // expect(multiply(453, 78)).to.equal(78 * 453);
+        expect(multiply(453, 78)).to.equal(78 * 453);
       });
 
       it('should use recursion by calling self', function() {
@@ -854,11 +854,11 @@
         expect(gcd(7, -36)).to.be.null;
         expect(gcd(-10, -58)).to.be.null;
         expect(gcd(-92, -5)).to.be.null;
-        // expect(gcd(0, 0)).to.be.null;
-        // expect(gcd(0, 5)).to.be.null;
-        // expect(gcd(5, 0)).to.be.null;
-        // expect(gcd(-5, 0)).to.be.null;
-        // expect(gcd(0, -5)).to.be.null;
+        expect(gcd(0, 0)).to.be.null;
+        expect(gcd(0, 5)).to.be.null;
+        expect(gcd(5, 0)).to.be.null;
+        expect(gcd(-5, 0)).to.be.null;
+        expect(gcd(0, -5)).to.be.null;
       });
 
       it('should use recursion by calling self', function() {
--- src/recursion.js
+++ src/recursion.js
@@ -136,16 +136,94 @@ var palindrome = function(string) {
 // modulo(17,5) // 2
 // modulo(22,6) // 4
 var modulo = function(x, y) {
+  if (y === 0) {
+    return NaN;
+  }
+
+  if (x === 0 || x === y) {
+    return 0;
+  }
+
+  if (x > y) {
+    if (y > 0) {
+      if (x - y >= y) {
+        return modulo(x-y, y);
+      } else {
+        return x - y;
+      }
+    } else if (x < 0) {
+      return x;
+    }
+  } else {
+    if (y < 0) {
+      if (x - y <= y) {
+        return modulo(x-y, y);
+      } else {
+        return x - y;
+      }
+    } else if (x > 0) {
+      return x;
+    } else if (x < 0 && y > 0) {
+      if (y <= -x) {
+        return modulo(x+y, y);
+      } else {
+        return x;
+      }
+    }
+  }
+
 };
 
 // 12. Write a function that multiplies two numbers without using the * operator or
 // Math methods.
 var multiply = function(x, y) {
+  if (x === 0 || y === 0) {
+    return 0;
+  } else if (x === 1) {
+    return y;
+  } else if (y === 1) {
+    return x;
+  } else if (x === -1) {
+    return -y;
+  } else if (y === -1) {
+    return -x;
+  }
+
+  if (y > 1) {
+    return x + multiply(x, y - 1);
+  } else if ( y < -1) {
+    return multiply(x, y + 1) - x;
+  }
 };
 
 // 13. Write a function that divides two numbers without using the / operator or
 // Math methods to arrive at an approximate quotient (ignore decimal endings).
 var divide = function(x, y) {
+  if (y === 0) {
+    return NaN;
+  }
+
+  if (x === 0) {
+    return 0;
+  }
+
+  if (x === y) {
+    return 1;
+  }
+
+  if (x > y) {
+    if (y > 0) {
+      if (x - y >= 0) {
+        return 1 + divide(x-y, y);
+      } else {
+        return x - y;
+      }
+    } else if (x < 0) {
+      return 0;
+    }
+  } else {
+    return 0;
+  }
 };
 
 // 14. Find the greatest common divisor (gcd) of two positive numbers. The GCD of two
@@ -154,6 +232,22 @@ var divide = function(x, y) {
 // http://www.cse.wustl.edu/~kjg/cse131/Notes/Recursion/recursion.html
 // https://www.khanacademy.org/computing/computer-science/cryptography/modarithmetic/a/the-euclidean-algorithm
 var gcd = function(x, y) {
+  if (x <= 0 || y <= 0) {
+    return null;
+  }
+
+  if (x % y === 0) {
+    return y;
+  }
+  if (x === y || y % x === 0) {
+    return x;
+  }
+
+  if ( x === 1 || y === 1) {
+    return 1;
+  }
+
+  return gcd(y, x % y);
 };
 
 // 15. Write a function that compares each character of two strings and returns true if
@@ -161,6 +255,15 @@ var gcd = function(x, y) {
 // compareStr('house', 'houses') // false
 // compareStr('tomato', 'tomato') // true
 var compareStr = function(str1, str2) {
+  if (str1 === '' && str2 === '') {
+    return true;
+  }
+
+  if (str1[0] !== str2[0]) {
+    return false;
+  }
+
+  return compareStr(str1.slice(1), str2.slice(1));
 };
 
 // 16. Write a function that accepts a string and creates an array where each letter
